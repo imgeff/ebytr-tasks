d8b7162c4949c2c46180ee2b192fb22aaa5946a4
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-07-04 11:29:26 -0300

    :sparkles: feat: Adiciona CORS na aplização
    para permitir o compartilhamento dos recursos

--- app/backend/src/app.js
+++ app/backend/src/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const userRoute = require('./routes/user.route');
 const taskRoute = require('./routes/task.route');
 const statusRoute = require('./routes/status.route');
 const { errorManager } = require('./middlewares/error');
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
