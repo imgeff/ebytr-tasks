91b43e59fe85eaa9a4c0dc711661433a366e107d
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-07-03 21:41:21 -0300

    :white_check_mark: test: Adiciona testes do endpoint POST /users
    verifica comportamento do endpoint na criação de um usuário com dados válidos e inválidos

---
+++ app/backend/src/tests/integration/user/data.js
@@ -0,0 +1,25 @@
+const user = {
+  "name": "teste",
+  "email": "teste@teste.com",
+  "password": "teste123"
+}
+
+const userDB = {
+  id: 3,
+  name: 'teste',
+  email: 'teste@teste.com',
+  password: 'teste123'
+}
+
+
+const userResponse = {
+	id: 3,
+	name: 'teste',
+	token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywibmFtZSI6InRlc3RlIiwiaWF0IjoxNjU2ODgzODg5LCJleHAiOjE2NTY4OTQ2ODl9.1ryKzQu903Z7U66nJR9c3mE5Jv0lGsbWzbvvQ79LjD0'
+}
+
+module.exports = {
+  user,
+  userDB,
+  userResponse,
+}
